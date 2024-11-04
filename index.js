d704a45bb8d6ddd17b2b48b85b4fd7813aed43cf
RUPAM SADHUKHAN <115567972+GRUMPY-TUCKER@users.noreply.github.com>
2024-11-04 21:25:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {Hello,Hello2,Bye} from './App';
+import {value} from './App';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
